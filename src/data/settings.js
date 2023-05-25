ce45ed0e66b7a69a7eaf881b06177ed1f100904f
tjex <tillmanjex@mailbox.org>
2023-05-25 12:44:12 +0200

    settings config

--- src/data/settings.js
+++ src/data/settings.js
@@ -4,10 +4,6 @@ export default {
   url: `https://tjex.net`, // No trailing slash!
   // JSON LD
   name: `tillman jex`,
-  // Facebook URL
-  //facebookURL: `https://facebook.com`,
-  // Twitter URL
-  //twitterURL: `https://twitter.com/jaydanurwin`,
-  // Instagram URL
-  //instagramURL: `https://instagram.com/jaydanurwin`,
+  githubURL: `https://github.com/tjex`,
+  mastodonURL: `https://fosstodon.org/@tjex`,
 };
