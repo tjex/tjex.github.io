8deacd7937ea441c7e4880fce5c8b773ce565471
tjex <tillmanjex@mailbox.org>
2023-05-25 16:53:48 +0200

    schema additions

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,20 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 // 2. Define your collection(s)
 const blogCollection = defineCollection({ 
     type: 'content', 
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        pubDate: z.string(),
+        pubDate: z.string().transform((str) => new Date(str)),
         author: z.string(),
+        authorContact: z.string().email(),
         tags: z.array(z.string()),
         category: z.string(),
-        image: z.string().optional(),
-        alt: z.string().optional(),
+        image: z.object({
+            src: z.string().optional(),
+            alt: z.string().optional(),
+        }
   }),
 });
 // 3. Export a single `collections` object to register your collection(s)
