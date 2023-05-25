a5af33fa627fe0154cb3c3c8a1b52344f5e66ec0
tjex <tillmanjex@mailbox.org>
2023-05-25 22:22:41 +0200

    added mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config"; // import lit from '@astrojs/lit';
 
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://tjex.net/",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
