4df6d4deb6409313adc50a2f68381ad4e4cc4cb5
tjex <tillmanjex@mailbox.org>
2023-06-01 08:00:15 +0200

    clean up settings and fix rss

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,10 +1,11 @@
 export default {
   title: `Tillman Jex`,
-  description: `Software developer, creative technologist and a few other things`,
+  description: `Software developer, creative technologist and animated meat sack`,
   url: `https://tjex.net`, // No trailing slash!
+  rssURL: `https://tjex.net/rss.xml`,
   // JSON LD
   webName: `tjex`,
-  rssName: `tjex.net`,
+  rssName: `tjex.net - blog`,
   githubURL: `https://github.com/tjex`,
   mastodonURL: `https://fosstodon.org/@tjex`,
 };
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,21 +1,27 @@
 import rss from '@astrojs/rss';
+import site from '../data/settings';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
+
 const parser = new MarkdownIt();
 
 export async function get(context) {
   const blog = await getCollection('blog');
   return rss({
     title: "tjex.net - blog",
-    description: "Software development and other thought excercises",
-    site: context.site,
+    description: site.description,
+    site: site.url,
+    customData: `
+      <atom:link href='${site.rssURL}' rel='self' type='application/rss+xml' 
+      xmlns:atom='http://www.w3.org/2005/Atom' xmlns:content='http://purl.org/rss/1.0/modules/content/'>
+      </atom:link>
+    `,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       categories: post.data.tags,
-      customData: post.data.customData, 
       link: `/blog/${post.slug}/`,
       content: sanitizeHtml(parser.render(post.body)),
     })),
