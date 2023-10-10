d544032794d510919bf636430a6f987c4c525029
Tillman Jex <83029642+tjex@users.noreply.github.com>
2023-10-10 12:56:18 +0200

    getBlog util (#11)
    * trying to get return value in [...tag]
    
    * get draft posts from util file working

--- src/components/utility/getBlog.js
+++ src/components/utility/getBlog.js
@@ -1,10 +1,10 @@
 import { getCollection } from "astro:content";
 
 export async function noDrafts() {
-let noDrafts = await getCollection("blog", ({ data }) => {
+const noDrafts = getCollection("blog", ({ data }) => {
     return data.draft !== true;
 });
-    export noDrafts
+    return noDrafts
 }
 
 export function test() {
