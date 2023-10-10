d0f06c01fe2116d808d9b3f1e8ffbbd88573509b
tjex <tjex@tjex.net>
2023-10-10 12:54:02 +0200

    get draft posts from util file working

--- src/components/utility/getBlog.js
+++ src/components/utility/getBlog.js
@@ -1,7 +1,7 @@
 import { getCollection } from "astro:content";
 
 export async function noDrafts() {
-let noDrafts = await getCollection("blog", ({ data }) => {
+const noDrafts = getCollection("blog", ({ data }) => {
     return data.draft !== true;
 });
     return noDrafts
