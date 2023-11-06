370a0ea129a433f38b8636cb1fdeaa5d359b07b2
tjex <tjex@tjex.net>
2023-11-06 20:07:46 +0100

    TagsUnique component refactor and design work

--- src/data/settings.js
+++ src/data/settings.js
@@ -3,9 +3,6 @@ export default {
   description: `Programmer, creative technologist and animated meat sack.`,
   url: `https://tjex.net`, // No trailing slash!
   rssURL: `https://tjex.net/rss.xml`,
-  // JSON LD
   webName: `tjex`,
   rssName: `tjex.net - blog`,
-  githubURL: `https://github.com/tjex`,
-  mastodonURL: `https://fosstodon.org/@tjex`,
 };
