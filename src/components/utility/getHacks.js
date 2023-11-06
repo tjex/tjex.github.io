9cdc444ab1f24c0cc9e4026cfda97854c75dea17
tjex <tjex@tjex.net>
2023-11-06 11:31:42 +0100

    finish replacement of getBlog.js with collectionUtil.js

--- src/components/utility/getHacks.js
+++
@@ -1,8 +0,0 @@
-import { getCollection } from "astro:content";
-
-export async function noDrafts() {
-const noDrafts = getCollection("hacks", ({ data }) => {
-    return import.meta.env.PROD ? data.draft !== true : true;
-});
-    return noDrafts
-}
