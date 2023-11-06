97060d8010ddf067fd7972aae4ffc05db8defc2e
tjex <tjex@tjex.net>
2023-11-06 17:08:21 +0100

    getEntriesPerTag working. Need to dyanmically deal with collection tag

--- src/scripts/collectionUtil.ts
+++ src/scripts/collectionUtil.ts
@@ -1,31 +1,55 @@
 import { getCollection } from "astro:content";
 
 export async function noDrafts(collection: string) {
-  const noDrafts = getCollection(collection, ({ data }) => {
-      return import.meta.env.PROD ? data.draft !== true : true;
+    const noDrafts = getCollection(collection, ({ data }) => {
+        return import.meta.env.PROD ? data.draft !== true : true;
     }
-  );
-  return noDrafts;
+    );
+    return noDrafts;
 }
 
-export async function getStaticPaths(collection: string) {
-    const entries = await noDrafts(collection);
+export async function getEntriesPerTag(collections: string[]) {
+    const l = []
+    for (const collection of collections) {
+        const entries = await noDrafts(collection);
 
-    const uniqueTags = [...new Set(entries.map((entry: any) => entry.data.tags).flat())];
+        const uniqueTags = [...new Set(entries.map((entry: any) => entry.data.tags).flat())];
 
-    return uniqueTags.map((tag) => {
-        const filteredentries = entries
-            .filter((entry) => entry.data.tags.includes(tag))
-            .map(({ data, slug }) => ({
-                slug: slug,
-                title: data.title,
-                description: data.description,
-                pubDate: data.pubDate,
-            }));
-        return {
-            params: { tag },
-            props: { entries: filteredentries },
-        };
-    });
+        const allFiltEntries = uniqueTags.map((tag) => {
+            const filtEntries = entries
+                .filter((entry) => entry.data.tags.includes(tag))
+                .map(({ data, slug }) => ({
+                    slug: slug,
+                    title: data.title,
+                    description: data.description,
+                    pubDate: data.pubDate,
+                    collection: collection,
+                }));
+            return {
+                params: { tag },
+                props: { entries: filtEntries },
+            };
+        });
+        l.push(...allFiltEntries);
+    }
+    return l
 }
 
+// export async function getEntriesPerTag(collection: string) {
+//     const entries = await noDrafts(collection);
+//
+//     const uniqueTags = [...new Set(entries.map((entry: any) => entry.data.tags).flat())];
+//
+//     return uniqueTags.map((tag) => {
+//         const filteredentries = entries
+//             .filter((entry) => entry.data.tags.includes(tag))
+//             .map(({ data, slug }) => ({
+//                 slug: slug,
+//                 title: data.title,
+//                 description: data.description,
+//                 pubDate: data.pubDate,
+//             }));
+//         return {
+//             params: { tag },
+//             props: { entries: filteredentries },
+//         };
