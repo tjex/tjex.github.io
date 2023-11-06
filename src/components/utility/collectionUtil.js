10988b87e50477b0fd860f346cde6de2b09e683b
tjex <tjex@tjex.net>
2023-11-06 11:14:46 +0100

    refactor code to create collectionUtil

--- src/components/utility/collectionUtil.js
+++ src/components/utility/collectionUtil.js
@@ -1,7 +1,7 @@
 import { getCollection } from "astro:content";
 
-export async function noDrafts() {
-const noDrafts = getCollection("blog", ({ data }) => {
+export async function noDrafts(collection) {
+const noDrafts = getCollection(collection, ({ data }) => {
     return import.meta.env.PROD ? data.draft !== true : true;
 });
     return noDrafts
