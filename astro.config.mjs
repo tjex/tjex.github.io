a91fb9f841d1df351e6297fcdc01bf1500ee8421
tjex <tjex@tjex.net>
2023-11-09 21:16:42 +0100

    rm workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://tjex.net",
     markdown: {
