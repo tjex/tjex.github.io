18f6a8b6de369432959520767f9ff7ece9b4e5bc
tjex <tjex@tjex.net>
2023-11-13 12:56:29 +0100

    .htaccess manual handling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 
 cd ${SCRIPT_DIR}/.. && yarn run build || exit
-rsync -acu --del --stats dist/* tjex.net@ssh.strato.de:tjex.net || exit
+cp src/.htaccess dist
+rsync -acu --del --stats dist/* tjex.net@ssh.strato.de:tjex.net
