19b5bbe1cbfaa790cc31f97312e1ec48195c8be5
tjex <tjex@tjex.net>
2024-04-11 15:54:21 +1000

    change rss title

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,8 +1,8 @@
 export default {
   title: `Tillman Jex`,
-  description: `Programmer, creative technologist and animated meat sack.`,
+  description: `Programmer and creative technologist.`,
   url: `https://tjex.net`, // No trailing slash!
   rssURL: `https://tjex.net/rss.xml`,
   webName: `tjex`,
-  rssName: `tjex.net - blog`,
+  rssName: `Tillman Jex : tjex.net`,
 };
