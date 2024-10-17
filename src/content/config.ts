9dc2c770b5df9ea0c2ec004b6e54c445a6b02875
tjex <tjex@tjex.net>
2024-10-17 15:52:57 +0200

    Removed hacks collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -21,22 +21,6 @@ const blog = defineCollection({
     }),
 });
 
-const hacks = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.date(),
-    modDate: z.date().optional(),
-    author: z.string(),
-    tags: z.array(z.string()),
-    draft: z.boolean(),
-    license: z.string().optional(),
-    progVersions: z.array(z.string()).optional(), // the version of the program in any given hack / tutorial
-  }),
-});
-
 export const collections = {
   blog: blog,
-  hacks: hacks,
 };
