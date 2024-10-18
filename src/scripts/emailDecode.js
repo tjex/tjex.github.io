bdd997cda4d13a3b098c8348bce9aa6a607cb5d4
tjex <tjex@tjex.net>
2024-10-18 19:46:35 +0200

    add reply via email

--- src/scripts/emailDecode.js
+++
@@ -1,5 +0,0 @@
-// from https://css-tricks.com/how-to-safely-share-your-email-address-on-a-website/#aa-the-javascript-approach
-// decodes my encoded email with base64
-var encEmail = "dGpleEB0amV4Lm5ldA==";
-const form = document.getElementById("denc");
-form.setAttribute("href", "mailto:".concat(atob(encEmail)));
